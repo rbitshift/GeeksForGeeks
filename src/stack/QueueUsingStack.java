26e00c359cc1849a627fc894250e13d6859573f5
mahanmanav <mahanmanav@gmail.com>
2016-05-17 20:14:16 +0530

    Implemented Queue using Two Stack (enQ and deQ not O(1))

---
+++ src/stack/QueueUsingStack.java
@@ -0,0 +1,50 @@
+package stack;
+
+public class QueueUsingStack<Item extends Comparable<Item>> {
+	private Stack<Item> stack1;
+	private Stack<Item> stack2;
+	
+	public QueueUsingStack() {
+		stack1 = new Stack<Item>();
+		stack2 = new Stack<Item>();
+	}
+	
+	public void enQueue(Item item) {
+		if(stack2.isEmpty()) {
+			stack1.push(item);
+		} else {
+			while(!stack2.isEmpty()) {
+				stack1.push(stack2.pop());
+			}
+			stack1.push(item);
+		}
+	}
+	
+	public Item deQueue() {
+		while(!stack1.isEmpty()) {
+			stack2.push(stack1.pop());
+		}
+		
+		return stack2.pop();
+	}
+	
+	public static void main(String[] args) {
+		Integer[] input = {5, 7, 3, 1, 17, 19};
+		QueueUsingStack<Integer> queue = new QueueUsingStack<Integer>();
+		queue.enQueue(input[0]);
+		queue.enQueue(input[1]);
+		System.out.println(queue.deQueue());
+		
+		queue.enQueue(input[2]);
+		System.out.println(queue.deQueue());
+		
+		queue.enQueue(input[0]);
+		queue.enQueue(input[4]);
+		queue.enQueue(input[5]);
+		
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+	}
+}
