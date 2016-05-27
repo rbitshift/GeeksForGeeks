c91e92d54056eaaf3b6e2e129442c4061758720a
mahanmanav <mahanmanav@gmail.com>
2016-05-27 08:38:20 +0530

    Minor Update

--- src/queue/GraphBFSTraversal.java
+++ src/queue/GraphBFSTraversal.java
@@ -4,53 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GraphBFSTraversal {
-	
-	public void printBFSPath(DirectedGraph G, int s) {
-		boolean[] marked = new boolean[G.V()];
-		Queue<Integer> queue = new Queue<Integer>();
-		
-		queue.enque(s);
-		marked[s] = true;
-		
-		while(!queue.isEmpty()) {
-			int v = queue.deque();
-			System.out.println(v);
-			for(int w : G.adj(v)) {
-				if(!marked[w]) {
-					queue.enque(w);
-					marked[w] = true;
-				}
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		DirectedGraph graph = new DirectedGraph(4);
-		graph.addEdge(0, 1);
-	    graph.addEdge(0, 2);
-	    graph.addEdge(1, 2);
-	    graph.addEdge(2, 0);
-	    graph.addEdge(2, 3);
-	    graph.addEdge(3, 3);
-	    
-	    GraphBFSTraversal traversal = new GraphBFSTraversal();
-	    traversal.printBFSPath(graph, 2);
-
-	}
-
-}
-
-class DirectedGraph {
 	private final int V;
 	private int E;
+	
 	private List<Integer>[] adj;
 	
-	public DirectedGraph(int V) {
+	public GraphBFSTraversal(int V) {
 		this.V = V;
-		adj = (List<Integer>[]) new ArrayList[V];
+		this.adj = (List<Integer>[]) new ArrayList[V];
 		
 		for(int i = 0; i < V; i++) {
-			adj[i] = new ArrayList<Integer>();
+			adj[i] = new ArrayList<Integer>(); 
 		}
 	}
 	
@@ -71,7 +35,7 @@ class DirectedGraph {
 		return adj[v];
 	}
 	
-	public String toString() {
+	public String printGraph() {
 		StringBuffer s = new StringBuffer();
 		s.append(V + " vertices " + E + " edges\n");
 		for(int v = 0; v < V; v++) {
@@ -82,5 +46,36 @@ class DirectedGraph {
 			s.append("\n");
 		}
 		return s.toString();
+	}
+	
+	public void printBFSPath(int s) {
+		boolean[] marked = new boolean[V];
+		Queue<Integer> queue = new Queue<Integer>();
+		
+		queue.enque(s);
+		marked[s] = true;
+		
+		while(!queue.isEmpty()) {
+			int v = queue.deque();
+			System.out.println(v);
+			for(int w : adj(v)) {
+				if(!marked[w]) {
+					queue.enque(w);
+					marked[w] = true;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		GraphBFSTraversal graph = new GraphBFSTraversal(4);
+		graph.addEdge(0, 1);
+	    graph.addEdge(0, 2);
+	    graph.addEdge(1, 2);
+	    graph.addEdge(2, 0);
+	    graph.addEdge(2, 3);
+	    graph.addEdge(3, 3);
+	    
+	    graph.printBFSPath(2);
 	}
 }
