ca62edd4447748a68ddb61312640cd6ff254e80e
mahanmanav <mahanmanav@gmail.com>
2016-07-03 11:52:38 +0530

    LIS Length and LCS Length Solution

--- src/dp/LongestCommonSubsequence.java
+++ src/dp/LongestCommonSubsequence.java
@@ -1,51 +1,88 @@
 package dp;
 
 public class LongestCommonSubsequence {
+	private static int[][] lcs;
 	
-	public static void solve(String a, String b) {
-		int[][] lcs = new int[a.length()+1][b.length()+1];
-		for(int i = 1; i <= a.length(); i++) {
-			for(int j = 1; j <= b.length(); j++) {
-				if(i == 0 || j == 0) {
-					lcs[i][j] = 0;
-				} else {
-					if(a.charAt(i-1) == b.charAt(j-1)) {
-						lcs[i][j] = 1 + lcs[i-1][j-1];
-					} else {
-						lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
-					}
-				}
+	public static int solve_recursive(String a, String b) {
+		return solve_recursive_helper(a, b,  a.length()-1, b.length()-1);
+	}
+	
+	private static int solve_recursive_helper(String strA, String strB, int posA, int posB) {
+		if(posA < 0 || posB < 0) {
+			return 0;
+		}
+		
+		if(strA.charAt(posA) == strB.charAt(posB)) {
+			return 1 + solve_recursive_helper(strA, strB, posA-1, posB-1);
+		} else {
+			return Math.max(
+				solve_recursive_helper(strA, strB, posA, posB-1),
+				solve_recursive_helper(strA, strB, posA-1, posB)
+			);
+		}
+	}
+	
+	public static int solve_memoization(String a, String b) {
+		lcs = new int[a.length()+1][b.length()+1];
+		for(int i = 0; i <= a.length(); i++) {
+			for(int j = 0; j <= b.length(); j++) {
+				if(i == 0 || j == 0) { lcs[i][j] = 0; 
+				} else { lcs[i][j] = -1; }
 			}
 		}
 		
-		StringBuilder sb = new StringBuilder();
+		return solve_memoization_helper(a, b, a.length(), b.length());
+	}
+	
+	private static int solve_memoization_helper(String strA, String strB, int posA, int posB) {
+		if(posA <= 0 || posB <= 0) {
+			return 0;
+		}
 		
-		int i = a.length(); int j = b.length();
-		while(i > 0 && j > 0) {
-			if(a.charAt(i-1) == b.charAt(j-1)) {
-				sb.insert(0, a.charAt(i-1));
-				i--; j--;
+		if(lcs[posA][posB] == -1) {
+			if(strA.charAt(posA-1) == strB.charAt(posB-1)) {
+				lcs[posA][posB] = 1 + solve_memoization_helper(strA, strB, posA-1, posB-1);
 			} else {
-				if(lcs[i][j-1] > lcs[i-1][j]) {
-					j--;
-				} else {
-					i--;
-				}
+				lcs[posA][posB] = Math.max(
+					solve_memoization_helper(strA, strB, posA, posB-1),
+					solve_memoization_helper(strA, strB, posA-1, posB)
+				);	
 			}
 		}
 		
-		System.out.println("LCS : " + sb.toString() + " Length : " + lcs[a.length()][b.length()]);		
+		return lcs[posA][posB];
+	}
+	
+	public static void printLCS(String strA, String strB) {
+		System.out.println("LCS LENGTH: " + solve_memoization(strA,  strB));
+		System.out.println("LCS STRING: " + printLCS(strA, strB, strA.length(), strB.length()));
+	}
+	
+	private static String printLCS(String strA, String strB, int posA, int posB) {
+		if(posA <= 0 || posB <= 0) {
+			return "";
+		} else if(strA.charAt(posA-1) == strB.charAt(posB-1)) {
+			return printLCS(strA, strB, posA-1, posB-1) + strA.charAt(posA-1);
+		} else {
+			if(lcs[posA][posB-1] > lcs[posA-1][posB]) {
+				return printLCS(strA, strB, posA, posB-1);
+			} else {
+				return printLCS(strA, strB, posA-1, posB);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		String a = "ABCDGH";
 		String b = "AEDFHR";
 		
-		LongestCommonSubsequence.solve(a, b);
+		assert LongestCommonSubsequence.solve_recursive(a, b) == 3;
+		LongestCommonSubsequence.printLCS(a, b);
 		
 		String c = "AGGTAB";
 		String d = "GXTXAYB";
 		
-		LongestCommonSubsequence.solve(c, d);
+		assert LongestCommonSubsequence.solve_recursive(c, d) == 4;
+		LongestCommonSubsequence.printLCS(c, d);
 	}
 }
--- src/dp/LongestIncreasingSubsequence.java
+++ src/dp/LongestIncreasingSubsequence.java
@@ -1,39 +1,64 @@
 package dp;
 
+//lis(n) = max(lisEndingAt(i)) where 0<=i<n; lisEndingAt(i) = (1 + lisEndingAt(j)) where aj < ai and 0<=j<i and lisEndingAt(0) = 1;
 public class LongestIncreasingSubsequence {
+	private static int[] lisend;
+	private static int[] lis;
 	
-	public static void solve(int[] a) {
-		int[] c = new int[a.length];
-		
-		int max = 0, maxindex = -1;
+	public static int solve_recursive(int[] a) {
+		int maxLIS = 0;
 		for(int i = 0; i < a.length; i++) {
-			c[i] = 1;
-			for(int j = 0; j < i; j++) {
-				if(a[j] < a[i] && c[i] < 1 + c[j]) {
-					c[i] = 1 + c[j];
+			maxLIS = Math.max(maxLIS, solve_recursive_helper(a, i));
+		}
+		return maxLIS;
+	}
+	
+	private static int solve_recursive_helper(int[] a, int pos) {
+		if(pos < 0) { return 0; }
+		
+		int endLIS = 1;
+		for(int i = 0; i < pos; i++) {
+			if(a[i] < a[pos]) {
+				endLIS = Math.max(endLIS,  1 + solve_recursive_helper(a, i));
+			}
+		}		
+		return endLIS;
+	}
+	
+	//O(n2)
+	public static int solve_memoization(int[] a) {
+		lis = new int[a.length];
+		lisend = new int[a.length];
+		
+		for(int i = 0; i < a.length; i++) {
+			lis[i] = 1; lisend[i] = -1;
+		}
+		
+		for(int i = 1; i < a.length; i++) {
+			lis[i] = Math.max(lis[i-1], solve_memoization_helper(a, i));
+		}
+		return lis[a.length-1];
+	}
+	
+	private static int solve_memoization_helper(int[] a, int pos) {
+		if(pos < 0) { return 0; }
+		
+		if(lisend[pos] == -1) {
+			int endLIS = 1;
+			for(int i = 0; i < pos; i++) {
+				if(a[i] < a[pos]) {
+					endLIS = Math.max(endLIS, 1 + solve_memoization_helper(a, i));
 				}
 			}
-			
-			if(max < c[i]) {
-				max = c[i];
-				maxindex = i;
-			}
+			lisend[pos] = endLIS;
 		}
 		
-		StringBuilder sb = new StringBuilder();
-		sb.insert(0,  a[maxindex] + " ");
-		for(int i = maxindex; i >= 0; i--) {
-			if(a[i] < a[maxindex] && c[i] == c[maxindex] - 1) {
-				sb.insert(0, a[i] + " ");
-				maxindex = i;
-			}
-		}
-		System.out.println(max);
-		System.out.println(sb.toString());
+		return lisend[pos];
 	}
 	
 	public static void main(String[] args) {
 		int[] a = {10, 22, 9, 33, 21, 50, 41, 60, 1};
-		LongestIncreasingSubsequence.solve(a);
+		assert LongestIncreasingSubsequence.solve_recursive(a) == 5;
+		assert LongestIncreasingSubsequence.solve_memoization(a) == 5;
 	}
 }
