ee58d3d33200b4ffa910e382a7e480eef25e33d2
mahanmanav <mahanmanav@gmail.com>
2016-07-04 20:42:02 +0530

    Solution to Maximum Subarray Sum Problem

--- src/dp/MaximumSubarraySum.java
+++ src/dp/MaximumSubarraySum.java
@@ -1,31 +1,46 @@
 package dp;
 
 public class MaximumSubarraySum {
-	private static int maxSum;
+	private static int maxSumR;
 	
 	public static int solve_recursive(int[] a) {
 		if(a.length < 0) {
-			System.out.println("EMPTY ARRAY");
+			System.out.println("Input array is empty");
 			return 0;
 		}
-		maxSum = a[0];
-		solve_recursive(a, a.length-1);
+		
+		maxSumR = a[0];
+		solve_recursive_helper(a, a.length-1);
+		return maxSumR;
+	}
+	
+	private static int solve_recursive_helper(int[] a, int pos) {
+		if(pos == 0) {
+			return a[0];
+		}
+		
+		int localMax = Math.max(a[pos], a[pos] + solve_recursive_helper(a, pos-1));
+		maxSumR = Math.max(localMax, maxSumR);
+		
+		return localMax;
+	}
+	
+	public static int solve_bottom_up(int[] a) {
+		int maxSum = Integer.MIN_VALUE;
+		int localMax = 0;
+		
+		for(int i = 0; i < a.length; i++) {
+			localMax = Math.max(a[i], a[i] + localMax);
+			maxSum = Math.max(localMax,  maxSum);
+		}
+		
 		return maxSum;
 	}
 	
-	private static int solve_recursive(int[] a, int pos) {
-		if(pos == 0) {
-			return a[pos];
-		}
-		
-		int max = Math.max(a[pos],  a[pos] + solve_recursive(a, pos-1));
-		maxSum = Math.max(maxSum,  max);
-		
-		return max;
-	}
-
+	
 	public static void main(String[] args) {
 		int[] a = {-2, -3, 4, -1, -2, 1, 5, -3};
-		System.out.println(MaximumSubarraySum.solve_recursive(a));
+		assert MaximumSubarraySum.solve_recursive(a) == 7;
+		assert MaximumSubarraySum.solve_bottom_up(a) == 7;
 	}
 }
