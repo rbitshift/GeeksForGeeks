9ff64e34d0076a7addf837399b4fde72d845ad06
mahanmanav <mahanmanav@gmail.com>
2016-07-04 23:47:20 +0530

    Solution to Edit Distance Problem

--- src/dp/EditDistance.java
+++ src/dp/EditDistance.java
@@ -1,25 +1,62 @@
 package dp;
 
 public class EditDistance {
+	private static int[][] edit;
 	
-	public static void solve(String a, String b) {
-		int[][] ed = new int[a.length()+1][b.length()+1];
+	
+	public static int solve_recursive(String a, String b) {
+		return solve_recursive_helper(a, b, a.length()-1, b.length()-1);
+	}
+	
+	private static int solve_recursive_helper(String a, String b, int aPos, int bPos) {
+		if(aPos < 0 ) {
+			return 1 + bPos;
+		}
 		
-		for(int i = 1; i <= a.length(); i++) {
-			for(int j = 1; j <= b.length(); j++) {
+		if(bPos < 0) {
+			return 1 + aPos;
+		}
+		
+		if(a.charAt(aPos) == b.charAt(bPos)) {
+			return solve_recursive_helper(a, b, aPos-1, bPos-1);
+		} else {
+			return 1 + min(solve_recursive_helper(a, b, aPos-1, bPos-1),
+					solve_recursive_helper(a, b, aPos, bPos-1),
+					solve_recursive_helper(a, b, aPos-1, bPos));
+		}
+	}
+	
+	public static int solve_memoized(String a, String b) {
+		edit = new int[a.length()+1][b.length()+1];
+		for(int i = 0; i < edit.length; i++) {
+			for(int j = 0; j < edit[0].length; j++) {
 				if(i == 0 || j == 0) {
-					ed[i][j] = 0;
-				}
-				
-				if(a.charAt(i-1) == b.charAt(j-1)) {
-					ed[i][j] = ed[i-1][j-1];
+					edit[i][j] = 0;
 				} else {
-					ed[i][j] = 1 + min(ed[i][j-1], ed[i-1][j], ed[i-1][j-1]);
+					edit[i][j] = -1;
 				}
 			}
 		}
 		
-		System.out.println(ed[a.length()][b.length()]);
+		return solve_memoized_helper(a, b, a.length(), b.length());
+	}
+	
+	private static int solve_memoized_helper(String a, String b, int aPos, int bPos) {
+		if(aPos == 0) {
+			edit[aPos][bPos] = bPos;
+		} else if(bPos == 0) {
+			edit[aPos][bPos] = aPos;
+		} else if(edit[aPos][bPos] == -1) {
+			if(a.charAt(aPos-1) == b.charAt(bPos-1)) {
+				edit[aPos][bPos] = solve_memoized_helper(a, b, aPos-1, bPos-1);
+			} else {
+				edit[aPos][bPos] = 1 + min(solve_memoized_helper(a, b, aPos-1, bPos-1),
+						solve_memoized_helper(a, b, aPos, bPos-1),
+						solve_memoized_helper(a, b, aPos-1, bPos));
+			} 
+		}
+		
+		return edit[aPos][bPos];
 	}
 	
 	private static int min(int a, int b, int c) {
@@ -27,8 +64,12 @@ public class EditDistance {
 	}
 
 	public static void main(String[] args) {
-		String a = "kitten";
-		String b = "sitting";
-		EditDistance.solve(a, b);
+		assert EditDistance.solve_recursive("cat", "cut") == 1;
+		assert EditDistance.solve_recursive("kitten", "sitting") == 3;
+		assert EditDistance.solve_recursive("saturday", "sunday") == 3;
+		
+		assert EditDistance.solve_memoized("cat", "cut") == 1;
+		assert EditDistance.solve_memoized("kitten", "sitting") == 3;
+		assert EditDistance.solve_memoized("sunday", "saturday") == 3;
 	}
 }
