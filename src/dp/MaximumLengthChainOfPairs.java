fa98b1a3c8e4af4c98710f58e14169c106cac1c6
mahanmanav <mahanmanav@gmail.com>
2016-08-06 10:33:44 +0530

    Solution to Maxlen of Chain with all rotations

--- src/dp/MaximumLengthChainOfPairs.java
+++ src/dp/MaximumLengthChainOfPairs.java
@@ -1,18 +1,30 @@
 package dp;
 
+import java.util.Arrays;
+
 public class MaximumLengthChainOfPairs {
 	private static int maxlenR;
 	
-	public static int solve_recursive(int[][] pairs) {
-		if(pairs.length <= 0) {
+	public static int solve_recursive(int[][] p) {
+		if(p.length <= 0) {
 			System.out.println("Invalid input");
 			return 0;
 		}
-		solve_recursive_helper(pairs, pairs.length-1);
-		return maxlenR;
-	}
+
+		Pair[] pairs = new Pair[p.length*2];
+        for (int i = 0; i < p.length ; i++) {
+            pairs[i*2] = new Pair(p[i][0], p[i][1]);
+            pairs[i*2+1] = new Pair(p[i][1], p[i][0]);
+        }
+		
+        Arrays.sort(pairs);
+        
+        maxlenR = 1;
+        solve_recursive_helper(pairs, pairs.length-1);
+        return maxlenR;
+    }
 	
-	public static int solve_recursive_helper(int[][] pairs, int pos) {
+	public static int solve_recursive_helper(Pair[] pairs, int pos) {
 		if(pos < 0) {
 			return 0;
 		}
@@ -29,15 +41,37 @@ public class MaximumLengthChainOfPairs {
 		return maxlink;
 	}
 	
-	private static boolean isLinkPossible(int[] p1, int[] p2) {
-		if(p1[0] < p1[1] && p2[0] < p2[1] && p2[0] < p1[1]) {
+	private static boolean isLinkPossible(Pair p1, Pair p2) {
+		if(p1.right < p2.left) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+	
+	private static void printPairs(Pair[] pairs) {
+		System.out.println("All possible combination of boxes after rotation");
+        for (int i = 0; i <pairs.length ; i++) {
+            System.out.println(pairs[i].left + " " + pairs[i].right);
+        }
+	}
 
 	public static void main(String[] args) {
-		System.out.println(MaximumLengthChainOfPairs.solve_recursive(new int[][]{{5, 24}, {39, 60}, {15, 28}, {27, 40}, {50, 90}}));
+		assert MaximumLengthChainOfPairs.solve_recursive(new int[][]{{5, 24}, {39, 60}, {15, 28}, {27, 40}, {50, 90}}) == 5;
+		assert MaximumLengthChainOfPairs.solve_recursive(new int[][]{{1, 5}, {2, 3}}) == 2;
+	}
+}
+
+class Pair implements Comparable<Pair> {
+	int left;
+	int right;
+	
+	public Pair(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	public int compareTo(Pair p) {
+		return right - p.right;
 	}
 }
